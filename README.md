d2827f8372dd4707fd8ddcd2f64c450f60e9c028
zvezdochiot <mykaralw@yandex.ru>
2020-06-27 11:37:48 +0300

    4.3.0: author plugins

---
+++ README.md
@@ -0,0 +1,44 @@
+# PhotoQuick plugins
+
+### Description
+A set of plugins for the viewer [photoquick] (https://github.com/ImageProcessing-ElectronicPublications/photoquick)
+
+### Runtime Dependencies
+* libqtcore4  
+* libqtgui4  
+* libgomp1
+
+### Build 
+
+#### Linux
+Install dependencies...  
+**Build dependencies ...**  
+ * libqt4-dev  
+
+To build this program, extract the source code zip.  
+Open terminal and change directory to src/  
+Then run these commands to compile...  
+```
+qmake  
+make -j4  
+```
+
+#### Windows
+Download Qt 4.8.7 and minGW32  
+Add Qt/4.8.7/bin directory and mingw32/bin directory in PATH environment variable.  
+In src directory open Command Line.  
+Run command...  
+```sh
+qmake
+make -j4
+```
+
+### Install (Linux)
+```sh
+sudo make install
+```
+
+### Links
+
+* https://github.com/ksharindam/photoquick
+* https://github.com/ImageProcessing-ElectronicPublications/photoquick
---
+++ src/canvas.h
@@ -0,0 +1,70 @@
+#pragma once
+/* Image Label Object to display the image. */
+
+#include <QLabel>
+#include <QMovie>
+#include <QMouseEvent>
+#include <QScrollArea>
+#include <QScrollBar>
+#include <QCursor>
+
+
+//This is the widget responsible for displaying image
+class Canvas : public QLabel
+{
+    Q_OBJECT
+public:
+    Canvas(QWidget *parent, QScrollArea *scrollArea);
+    void setAnimation(QMovie *anim);
+    void setImage(QImage img);
+    void rotate(int degree, Qt::Axis axis=Qt::ZAxis);
+    // Variables
+    QImage image;
+    bool animation;
+    float scale;
+    bool drag_to_scroll;    // if click and drag moves image
+    QString filename; // required by plugins
+private:
+    void mousePressEvent(QMouseEvent *ev);
+    void mouseReleaseEvent(QMouseEvent *ev);
+    void mouseMoveEvent(QMouseEvent *ev);
+    // Variables
+    bool mouse_pressed;
+    int v_scrollbar_pos, h_scrollbar_pos;
+    QPoint clk_global;
+    QScrollBar *vScrollbar, *hScrollbar;
+public slots:
+    void showScaled();
+signals:
+    void mousePressed(QPoint);
+    void mouseReleased(QPoint);
+    void mouseMoved(QPoint);
+    void imageUpdated();
+};
+
+
+// It is a drawing area for some dialogs
+
+class PaintCanvas : public QLabel
+{
+    Q_OBJECT
+public:
+    PaintCanvas(QWidget *parent);
+    void mousePressEvent(QMouseEvent *ev);
+    void mouseMoveEvent(QMouseEvent *ev);
+    void mouseReleaseEvent(QMouseEvent *ev);
+    void setImage(QImage image);
+signals:
+    void mousePressed(QPoint);
+    void mouseMoved(QPoint);
+    void mouseReleased(QPoint);
+};
+
+QCursor roundCursor(int width);
+
+// a QList<HistoryItem> is used as Undo or Redo stack
+typedef struct {
+    int x;
+    int y;
+    QImage image;// this image is replaced by new image at pos (x,y)
+} HistoryItem;
