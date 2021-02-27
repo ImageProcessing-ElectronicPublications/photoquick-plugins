ea9af577e389a5c51efc0600586517dca895ffea
zvezdochiot <mykaralw@yandex.ru>
2021-02-27 17:53:52 +0300

    4.3.7: bimodal: fixed loop and median

--- src/threshold/bimodal-threshold/bimodal_thresh.cpp
+++ src/threshold/bimodal-threshold/bimodal_thresh.cpp
@@ -16,7 +16,8 @@ Q_EXPORT_PLUGIN2(bimodal_thresh, FilterPlugin);
 
 int histogram_darkest(long long hist[])
 {
-    for (int i=0; i<256; i++) {
+    for (int i=0; i<256; i++)
+    {
         if (hist[i]!=0)
             return i;
     }
@@ -24,25 +25,35 @@ int histogram_darkest(long long hist[])
 }
 int histogram_lightest(long long hist[])
 {
-    for (int i=255; i>0; i--) {
+    for (int i=255; i>0; i--)
+    {
         if (hist[i]!=0)
             return i;
     }
     return 0;
 }
 
-float threshold_bimod(long long hist[], int tsize, float tpart) {
-    float T = tsize * tpart;
-    float Tn = 0;
-    while (T != Tn) {
+float threshold_bimod(long long hist[], int tsize, float tpart)
+{
+    int c;
+    long long Tb, Tw, ib, iw;
+    float tpartrev = 1.0 - tpart;
+    int T = (int)(tsize * tpart + 0.5), Tn = 0;
+
+    while (T != Tn)
+    {
         Tn = T;
-        long long Tb=0, ib=0;
-        for (int c=0; c<int(T + 0.5); c++) {
+        Tb = 0;
+        ib = 0;
+        for (c = 0; c < T; c++)
+        {
             Tb += hist[c] * c;
             ib += hist[c];
         }
-        long long Tw=0, iw=0;
-        for (int c=int(T + 0.5); c<int(tsize); c++) {
+        Tw=0;
+        iw=0;
+        for (c = T; c < tsize; c++)
+        {
             Tw += hist[c] * c;
             iw += hist[c];
         }
@@ -53,7 +64,7 @@ float threshold_bimod(long long hist[], int tsize, float tpart) {
         else if (ib == 0)
             T = Tw / iw;
         else
-            T = ((Tw / iw) * tpart + (Tb / ib) * (1.0 - tpart));
+            T = (int)((Tw / iw) * tpart + (Tb / ib) * tpartrev);
     }
     return T;
 }
@@ -61,30 +72,49 @@ float threshold_bimod(long long hist[], int tsize, float tpart) {
 // get threshold values for a single channel
 void thresholdBimodChannel(long long hist[], int thresval[], int tcount, int tdelta, bool median)
 {
+    int t, tt;
+    long long sh, sht;
     int thres[256] = {-1}; //tcount+1 for c++11 compiler
+    int newval[256] = {}; //tcount
 
-    for (int tt=1; tt<tcount; tt++) {
+    for (tt = 1; tt < tcount; tt++)
+    {
         float part = (float)tt / tcount;
         thres[tt] = int(threshold_bimod(hist, 256, part) + 0.5 + tdelta);
     }
-    int newval[256] = {}; //tcount
 
-    if (median) {
+    if (median)
+    {
         thres[0] = histogram_darkest(hist);
         thres[tcount] = histogram_lightest(hist); // get max
-        for (int tt=0; tt<tcount; tt++) {
-            newval[tt] = Clamp((thres[tt] + thres[tt + 1]) / 2);
+        for (tt = 0; tt < tcount; tt++)
+        {
+            sh = 0;
+            sht = 0;
+            for (t = thres[tt]; t <= thres[tt + 1]; t++)
+            {
+                sh += hist[t];
+                sht += (hist[t] * t);
+            }
+            if (sh > 0)
+            {
+                newval[tt] = Clamp(sht / sh);
+            } else {
+                newval[tt] = Clamp((thres[tt] + thres[tt + 1]) / 2);
+            }
         }
         thres[0] = -1;
-    }
-    else {
-        for (int tt=0; tt<tcount; tt++) {
+    } else {
+        for (tt = 0; tt < tcount; tt++)
+        {
             newval[tt] = Clamp((255 * tt) / (tcount - 1));
         }
     }
 
-    for (int t=0; t<256; t++) {
-        for (int tt=0; tt<tcount; tt++) {
+    for (t = 0; t < 256; t++)
+    {
+        for (tt = 0; tt < tcount; tt++)
+        {
             if (t > thres[tt])
                 thresval[t] = newval[tt];
         }
@@ -97,25 +127,28 @@ void thresholdBimod(QImage &img, int tcount, int tdelta, bool median)
     int imgH = img.height();
     // Calc Histogram
     long long hist[3][256] = {};
-    for (int y=0; y<imgH; y++)
+    int thresval[3][256] = {};
+    for (int y = 0; y < imgH; y++)
     {
         QRgb *row = (QRgb*)img.constScanLine(y);
-        for (int x=0; x<imgW; x++) {
+        for (int x = 0; x < imgW; x++) {
             ++hist[0][qRed(row[x])];
             ++hist[1][qGreen(row[x])];
             ++hist[2][qBlue(row[x])];
         }
     }
-    int thresval[3][256] = {};
 
     #pragma omp parallel for
-    for (int i=0; i<3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         thresholdBimodChannel(hist[i], thresval[i], tcount, tdelta, median);
     }
     // apply threshold to each pixel
-    for (int y=0; y<imgH; y++) {
+    for (int y = 0; y < imgH; y++)
+    {
         QRgb *row = (QRgb*)img.constScanLine(y);
-        for (int x=0; x<imgW; x++) {
+        for (int x = 0; x < imgW; x++)
+        {
             int clr = row[x];
             int r = thresval[0][qRed(clr)];
             int g = thresval[1][qGreen(clr)];
@@ -169,7 +202,8 @@ QString FilterPlugin:: menuItem()
 void FilterPlugin:: onMenuClick()
 {
     BimodThreshDialog *dlg = new BimodThreshDialog(data->window);
-    if (dlg->exec()==QDialog::Accepted) {
+    if (dlg->exec()==QDialog::Accepted)
+    {
         int count = dlg->countSpin->value();
         int delta = dlg->deltaSpin->value();
         thresholdBimod(data->image, count, delta, dlg->medianBtn->isChecked());
